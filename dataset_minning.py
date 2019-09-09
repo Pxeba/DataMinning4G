d4f062e489980145e10c26e04b7b3f9fa254311f
Pedro Henrique Mattiello <Pedromattiello@gmail.com>
2019-09-09 20:19:28 -0300

    adding file reader

--- dataset_minning.py
+++ dataset_minning.py
@@ -1,53 +1,77 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from enum import Enum 
+import os
+
 
 
 class Plots(Enum):
     THROUGHTPUT_DISTANCE = 1
     THROUGHTPUT_SPEED = 2
 
-def main(): 
-    data = pd.read_csv('A_2017.11.24_14.34.43.csv', na_values=['-']).dropna()
-    data['State'] = data['State'].replace('D', 1)
-    data['State'] = data['State'].replace('I', 0)
-    #context
-    speed_series = data['Speed']
-    distance_series = data['ServingCell_Distance']
-    #channel
-    cqi_series = data['CQI']
-    #coverage
-    rsrp_series = data['RSRP']
-    rsrq_series = data['RSRQ']
-    snr_series = data['SNR']
-    rssi_series = data['RSSI']
-    #throughtput    
-    dl_series = data['DL_bitrate']
-    ul_series = data['UL_bitrate']
+class DataMinning4G:
+    def start(self): 
 
-    print('speed: {}, distance: {}, cqi: {}, rsrp: {}, rsrq: {}, snr: {}, rssi: {}, dl: {}, ul: {} '.format(speed_series.mean(skipna = True),distance_series.mean(skipna = True),cqi_series.mean(skipna = True)
-    ,rsrp_series.mean(skipna = True),rsrq_series.mean(skipna = True),snr_series.mean(skipna = True),rssi_series.mean(skipna = True),dl_series.mean(skipna = True),ul_series.mean(skipna = True)))
+        path = '\\'
 
-    while(True):
-        getPlot(data)
-        plt.show()
+        files = {}
+        # r=root, d=directories, f = files
+        currentRoot = '-1'
+        for r, d, f in os.walk(".", topdown=False):
+            if 'git' in r:
+                continue
+            r  = r[2:]
+            if currentRoot != r:
+                currentRoot = r
+                files[r] = []
+            for file in f:
+                if '.csv' in file:
+                    files[r].append(file) 
 
-def getPlot(data):
-    #get user input
-    for idx, col in enumerate(data.columns.values):
-        print('{}: {}\n'.format(idx,col))
-    col1 = int(input('Selecione 2 colunas para vizualização:'))
-    for idx, col in enumerate(data.columns.values):
-        print('{}: {}\n'.format(idx,col))
-    col2 = int(input('Selecione 1 coluna para vizualização:'))
+        print('f {} d {} r {}\n'.format(f,d,r))
 
-    order_col_index = int(input('qual coluna será ordenada?(1 ou 2)'))
-    colx = col1 if order_col_index == 1 else col2
-    coly = col1 if order_col_index == 2 else col2
+            
+        # data = pd.read_csv('A_2017.11.24_14.34.43.csv', na_values=['-']).dropna()
+        # data['State'] = data['State'].replace('D', 1)
+        # data['State'] = data['State'].replace('I', 0)
+        # #context
+        # speed_series = data['Speed']
+        # distance_series = data['ServingCell_Distance']
+        # #channel
+        # cqi_series = data['CQI']
+        # #coverage
+        # rsrp_series = data['RSRP']
+        # rsrq_series = data['RSRQ']
+        # snr_series = data['SNR']
+        # rssi_series = data['RSSI']
+        # #throughtput    
+        # dl_series = data['DL_bitrate']
+        # ul_series = data['UL_bitrate']
 
-    data = data.sort_values(data.columns.values[colx])
-    data.plot(x=data.columns.values[colx], y=data.columns.values[coly])
+        # print('speed: {}, distance: {}, cqi: {}, rsrp: {}, rsrq: {}, snr: {}, rssi: {}, dl: {}, ul: {} '.format(speed_series.mean(skipna = True),distance_series.mean(skipna = True),cqi_series.mean(skipna = True)
+        # ,rsrp_series.mean(skipna = True),rsrq_series.mean(skipna = True),snr_series.mean(skipna = True),rssi_series.mean(skipna = True),dl_series.mean(skipna = True),ul_series.mean(skipna = True)))
+
+        # while(True):
+        #     self.getPlot(data)
+        #     plt.show()
+
+    def getPlot(self, data):
+        #get user input
+        for idx, col in enumerate(data.columns.values):
+            print('{}: {}\n'.format(idx,col))
+        col1 = int(input('Selecione 2 colunas para vizualização:'))
+        for idx, col in enumerate(data.columns.values):
+            print('{}: {}\n'.format(idx,col))
+        col2 = int(input('Selecione 1 coluna para vizualização:'))
+
+        order_col_index = int(input('qual coluna será ordenada?(1 ou 2)'))
+        colx = col1 if order_col_index == 1 else col2
+        coly = col1 if order_col_index == 2 else col2
+
+        data = data.sort_values(data.columns.values[colx])
+        data.plot(x=data.columns.values[colx], y=data.columns.values[coly])
 
 if __name__ == "__main__":
-    main()
+    m = DataMinning4G()
+    m.start()
 
